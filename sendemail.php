3b9852dfccf98c997fe32c50494c38bf93507404
yrshlyk <yrshlyk@gmail.com>
2022-03-17 15:29:41 +0300

    big update

---
+++ sendemail.php
@@ -0,0 +1,21 @@
+<?php
+	header('Content-type: application/json');
+	$status = array(
+		'type'=>'success',
+		'message'=>'Ваше сообщение отправлено!'
+	);
+
+    $name = @trim(stripslashes($_POST['name'])); 
+    $email = @trim(stripslashes($_POST['email'])); 
+    $subject = 'Покупатель с сайта по плитке'; 
+    $message = @trim(stripslashes($_POST['message'])); 
+
+    $email_from = $email;
+    $email_to = 'mail@cccp.ml';
+
+    $body = 'Имя отправителя: ' . $name . "\n" . 'Email: ' . $email . "\n\n" . 'Сообщение: ' . $message;
+
+    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+
+    echo json_encode($status);
+    die; 
